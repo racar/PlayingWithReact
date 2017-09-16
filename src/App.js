6e2beee1bb71a28ab90dac6a0a89e8e583e0b04f
Rafael Carrascal R <rcarrascal@gmail.com>
2017-09-16 18:42:53 -0500

    children also can work as props

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <br/><h1>{this.props.children}</h1>
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<div>
-                  <App text="original"/>
+                  <App text="original">I am the first copy</App>
                   <App text="copy"/>
                 </div>, document.getElementById('root'));
 registerServiceWorker();
